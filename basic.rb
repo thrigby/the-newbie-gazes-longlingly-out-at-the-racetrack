38b8e021e092b79ed783de226b2076205a6099a1
thugrigby <plutonoodle.com>
2010-12-13 20:25:56 -0800

    oop

---
+++ basic.rb
@@ -0,0 +1,97 @@
+module Basic         #basic commands where there isn't a direct room interaction
+
+
+  def do_help
+    send "                BABY SEAL MUD COMMANDS"
+    send "          say: just type say and your message"
+    send "         Look: examine your immediate surroundings."
+    send "         exit: leave the game"
+    send "         help: what you're looking at right now."
+    send "       Bounce: makes magic bubblegum. also how you move."
+    send "         Flop: for resting and chewing bubblegum."
+    send "          Get: pick up magic bubblegum."
+    send "         Chew: prepare for magic! precurser to blow."
+    send "         Blow: make powerful tools to aid you!"
+    send "         Take: picks up the most recent item you made."
+    send "    Inventory: examine your inventory."
+    send "         Wear: wield that magic!"
+    send "       Remove: un-wield that magic!"
+    send "         Drop: you drop a magic item on the ground."
+    send "         glue: glue two objects together. note- if you have three or more objects in your inventory, you'll lose them."
+    send " Capitalized commands have shortcuts, the first letter of the command. l for look, w for wear, and so on. note: b = bounce, bb = blow bubble"
+    send "\n Score or 'sc' gives you some vague information that will probably not be all that helpful. This game is completely rigged and you really don't stand any sort of chance."             
+  end
+  
+  def do_score
+    send "             Blubber: #{@str}"
+    send "       Eye-Beadiness: #{@dex}"
+#    send "Vim: #{@con}"
+    send "               Pluck: #{@cool}"
+    send "            Cuteness: #{@luck}"
+    send "      Whisker Length: #{@wis}"
+    send "                                XP: #{@exp}"     
+  end  
+
+  def do_flop
+    send "You grunt adorably as you flop down on the ground to rest."
+    other_players.each { |p| p.send "#{name} flops down onto the ground with an adorable little grunt."}
+    @position = true
+  end
+ 
+  def do_wear
+    if @inv.empty?
+      send "Your inventory is empty."
+    else
+      send "You carefully balance #{@inv.last} on your head."
+      other_players.each { |p| p.send "#{name} carefully balances a #{@inv.last} on his head. "}
+    @wear.push(@inv.pop)
+    end
+  end
+
+  def do_remove
+    if @wear.empty?
+      send "You aren't wearing anything to remove."
+    else
+      send "You remove a #{@wear.last.itemcolor} #{@wear.last.itemname} from your head. "
+      other_players.each { |p| p.send "#{name} removes a  #{@wear.last.itemcolor} #{@wear.last.itemname} from his head. "}
+    @inv.push(@wear.pop)
+    end
+  end
+
+  def do_chew
+    unless @position
+      send "One cannot chew gum and bounce at the same time."
+    return
+    end      
+    if @inventorybg > 0
+      send "You begin chewing one of your magic pieces of bubblegum."
+      other_players.each { |p| p.send "#{name} begins chewing a piece of magic bubblegum."}
+      @inventorybg -= 1
+      @readytoblow = true
+    else
+      send "You are all out of gum! Bounce to make more!"
+    end
+  end
+
+  def do_inventory
+    send "you look in your fannypack! you have #{@inventorybg} pieces of bubblegum!"
+    if @inv.empty?
+      send "You aren't carrying anything."
+    else
+       send "You are carrying:"
+       @inv.each { |m| send "#{m.itemcolor} #{m.itemname}"}  
+       
+       #{@room.item.last}    
+    end
+
+    if @wear.empty?
+      send "You aren't wearing anything."
+    else
+      send "\n You are balancing the following on your head:"
+      @wear.each { |m| send "#{m.itemcolor} #{m.itemname}"}     
+    end  
+      send "You sense that your attack power is #{@inv.length}"
+      send "You sense that your defense is #{@wear.length}"
+  end       
+
+end
--- mud.rb
+++ mud.rb
@@ -1,34 +1,84 @@
 require 'eventmachine'
-require 'magicalitems'
+require 'basic'
+#require 'loc'
 
-#push, pop, rdoc, detect
+=begin
+things to study: push*, pop*, rdoc*, detect (*more or less studied)
+
+1.  Use select or reject if you need to select or reject items based on a condition.
+2.  Use collect if you need to build an array of the results from logic in the block.
+3.  Use inject if you need to accumulate, total, or concatenate array values together.
+4.  Use detect if you need to find an item in an array.
+
+=end
+
 
 module MUD
+#  include Loc
   Brand = "BABY SEAL MUD!"
   Port = 8888
-  Players = []
-  Room = {:bubblegum => 0, :item => []}
+
+  class World
+    attr_accessor :number
+
+    def initialize (number)
+      @number = []
+    end
+  end
+
+  class Room
+    attr_accessor :bubblegum, :item, :players, :desc, :title, :number
+    
+    def initialize (title, desc, number)
+      @bubblegum = 0
+      @item = []
+      @players = []
+      @desc = desc
+      @title = title
+      @number = 0 
+    end      
+  end  
+
+
+
+  StartRoom = Room.new "THE ARCTIC", "You are on a beautiful, icy, rocky beach.", 1
+
  
   class Player
-    attr_accessor :name, :hp, :vit, :con, :dirty, :bounce
+    include Basic                      #basic non-room related commands
+    attr_accessor :name, :hp, :vit, :con, :dirty, :bounce, :wear
 
     def initialize(name, con)
       @name = name
-      @con = con
       @hp = 10
+      @con = con
       @vit = 12
       @dirty = true
-      @inventorybg = 4
+      @inventorybg = 10
+      @inv = []
+      @wear = []
+      @str = rand(10) + 1
+      @dex = rand(10) + 1
+#      @con = rand(10) + 1
+      @cool = rand(10) + 1
+      @luck = rand(10) + 1
+      @wis = rand(10) + 1
+      @exp = 0
       @position = true
       @readytoblow = false
-      @room = 1
-      @inv = [] #internal player inventory array
-      @wear = [] #wearing things!
-      MUD::Players << self
-      send "Welcome #{name}! You are a baby seal!"
-      other_players.each { |p| p.send "#{name} has arrived." }
+      send "Welcome #{name}! You are a baby seal!"         
       prompt
+#      room_array
     end
+
+    def to_room(room)
+      @room = room
+      @room.players << self
+      other_players.each { |p| p.send "#{name} has arrived." }
+      #attach the room to the player
+      #attach the player to the room
+      
+    end 
 
     def send(data)
       unless @dirty
@@ -41,96 +91,206 @@ module MUD
     end
 
     def other_players
-      MUD::Players.reject { |p| p == self }
+#      MUD::Players.reject { |p| p == self }
+       @room.players.reject { |p| p == self}      
+#      array.reject {|item| block } → an_array
+#      Returns a new array containing the items in self for which the block is not true.      
     end
 
     def command(cmd, arg)
       @dirty = true
       case cmd
+        when "digwest"; do_dig_west
+        when "smile"; do_smile(arg)
+        when "west"; do_west
+        when "sc"; do_score
+        when "score"; do_score
+        when "exa"; do_exa(arg)
         when "say"; do_say(arg)
         when "look"; do_look
-        when "take"; do_take
-        when "drop"; do_drop
-        when "wear"; do_wear
+        when "l"; do_look
         when "exit"; do_exit
-        when "help"; do_help
-        when "chew"; do_chew
+        when "help"; do_help         #basic
+        when "drop"; do_drop
+        when "d"; do_drop
+        when "wear"; do_wear         #basic
+        when "w"; do_wear
+        when "remove"; do_remove     #basic
+        when "r"; do_remove
+        when "take"; do_take
+        when "t"; do_take          
+        when "chew"; do_chew         #basic
+        when "c"; do_chew
         when ""; ## do_nothing
         when "bounce"; do_bounce
-        when "flop"; do_flop
+        when "b"; do_bounce
+        when "flop"; do_flop         #basic
+        when "f"; do_flop
         when "get"; do_get
+        when "g"; do_get
         when "blow"; do_blow
-        when "inventory"; do_inventory
+        when "bb"; do_blow
+        when "inventory"; do_inventory    #basic
+        when "i"; do_inventory
+        when "glue"; do_glue
         else ; send "Unknown Command: '#{cmd}'. Type 'help' for commands."
       end
-      MUD::Players.each { |p| p.prompt if p.dirty }
+ #     MUD::Players.each { |p| p.prompt if p.dirty }
+       @room.players.each { |p| p.prompt if p.dirty }
     end
 
-    def do_look
-      send "THE ARCTIC"
-      send "You are on a beautiful, icy, rocky beach."
-      send "#{Room[:bubblegum]} pieces of bubblegum sit here."
-      send " [------]"
-      other_players.each { |p| send "#{p.name} is here." }     
-      Room[:item].each { |m| send "#{m.itemcolor} #{m.itemname} is here.".capitalize}
+
+#array.inspect → string...detect!
+    def find_player_by_name(name)  
+      @room.players.detect { |p| p.name.downcase == name.downcase}
     end
+
+    def do_dig_west
+      new_room = Room.new "Ice Tunnel", "It's cold.", 2
+      send "You use your baby seal magic powers and dig westward!"
+      send "You have created a new room!"
+      other_players.each { |p| p.send "#{name} digs a new room to the west."}
+    end
+
+    def do_exa(name)
+      target = find_player_by_name(name)
+      if target
+        send "You examine #{target.name}."
+        send "They have #{target.wear.size} things."
+        target.wear.each do |item| 
+          send "#{item}"
+        end        
+      else
+        send "NOBODY HOME"
+      end  
+    end
+
+    def do_smile(name)
+      target = find_player_by_name(name)
+      if target
+        send "You smile at #{target.name}."
+        end        
+      else
+        send "NOBODY HOME"
+      end  
+    end
+
 
     def do_say(message)
       send "You say '#{message}'"
       other_players.each { |p| p.send "#{name} says '#{message}'" }
     end
 
+    def do_look
+      send "#{@room.title}"
+      send "#{@room.desc}"
+      send "#{@room.bubblegum} pieces of bubblegum sit here."
+      send " [------]"
+      other_players.each { |p| send "#{p.name} is here." }
+      @room.item.each { |m| send "A #{m.itemcolor} #{m.itemname} is here."}
+    end
+
     def do_exit
       send "A Polar Bear Eats You!"
       other_players.each { |p| p.send "#{name} was dragged away by a polar bear!" }
       con.close_connection_after_writing
-      MUD::Players.delete self
+#      MUD::Players.delete self
+       @room.players.delete self    #!!!!!!!!!
     end
 
-    def do_help
-      send "Commands: say, look, exit, help, make, bounce, flop, blow, take, inventory, drop"
+    def do_drop
+      if @inv.empty?
+        send "You aren't carrying anything."
+      else
+        send "You drop #{@inv.last.itemcolor} #{@inv.last.itemname} on the ground."
+        other_players.each { |p| p.send "#{name} drops a #{@inv.last.itemcolor} #{@inv.last.itemname} ."}
+        @room.item.push(@inv.pop)
+      end
+    end
+
+    def do_take
+      if @room.item.empty?
+         send "empty room"
+      else
+        send "You flip #{@room.item.last} onto your back."
+        other_players.each { |p| p.send "#{name} flips #{@room.item.last} on his back."}
+        @inv.push(@room.item.pop)             
+      end 
     end
 
     def do_bounce
       send "You start bouncing up and down!"
       other_players.each { |p| p.send "#{name} starts bouncing up and down!"}
-      bouncygum = rand(4) + 1
-      Room[:bubblegum] += bouncygum
+      bouncygum = rand(4) + 1   
+      @room.bubblegum += bouncygum
       @position = false
       send "You make #{bouncygum} magic pieces of bubblegum!"
-      other_players.each { |p| p.send "#{name} makes #{bouncygum} magic pieces of bubblegum!" }
+      other_players.each { |p| p.send "#{name} makes #{bouncygum} magic pieces of bubblegum!" }  
     end
 
-    def do_chew
-      unless @position
-        send "One cannot chew gum and bounce at the same time."
-      return
-      end
-      if @inventorybg > 0
-        send "You begin chewing one of your magic pieces of bubblegum."
-        other_players.each { |p| p.send "#{name} begins chewing a piece of magic bubblegum."}
-        @inventorybg -= 1
-        @readytoblow = true
+    def do_get
+      if @room.bubblegum == 0
+        send "There is no bubblegum here!"
       else
-        send "You are all out of gum! Bounce to make more!"
+        send "You pick up #{@room.bubblegum} pieces of magical bubblegum."
+        other_players.each { |p| p.send "#{name} picks up #{@room.bubblegum} pieces of magical bubblegum."}
+        @inventorybg += @room.bubblegum
+        @room.bubblegum = 0
       end
     end
-   
-    def do_flop
-      send "You grunt adorably as you flop down on the ground to rest."
-      other_players.each { |p| p.send "#{name} flops down onto the ground with an adorable little grunt."}
-      @position = true
-    end
 
+    def do_blow
+      unless @readytoblow
+        send "You must chew the bubblegum before you can blow a bubble!"
+      else
+        send "You blow a bright red, magic bubble until it POPS!"
+        other_players.each { |p| p.send "#{name} blows a bright red magic bubble until it POPS!"}
+        @readytoblow = false
+          color = ["red-painted", "green-painted", "blue-painted", "bloody", "rusty", "used", "rancid", "holy", "rubber", "ragged", "moldy", "tasteful", "ceremonial", "shag-carpeted", "dusty", "rotting", "solid-gold"]
+          cc = color[rand(color.size)]
+          toy = ["gimp suit", "stapler", "needle", "raincloud", "superman suit", "box jellyfish", "polar bear chew toy", "cardboard-cutout of Matthew McConaughey", "balloon", "harpoon", "battle-robot", "toy yeti", "home-liposuction kit", "polar-bear pelt", "jar of pickles", "batman cowl", "pixie wings", "hula-hoop", "glow sticks", "shrunken head", "vader helmet", "wonder-woman suit", "teacup", "dead baby seal", "Lady Liberty crown and torch", "collectable spoon", "witch's hat", "twelve-gauge shotgun", "piece of Einstein's brain", "talking stick", "tommygun", "underwood typewriter", "boquet of roses", "umbrella", "ring of power", "inflatable Silvia Plath doll", "egyptian mummy"]
+          tt = toy[rand(toy.size)]
+          send "A #{cc} #{tt} appears!"
+          other_players.each { |p| p.send "A #{cc} #{tt} appears!"}   
+          @room.item << MagicalItem.new("#{tt}", "#{cc}")       
+      end
+    end
+     
+    def do_glue
+      unless @readytoblow
+        send "You must chew the gum before you can use it as glue!"
+      else
+        if @inv.nitems < 2    #simplify so only two items in inventory
+          send "You need at least two things in your inventory to glue together."
+        else
+          send "You carefully glue a #{@inv[0].itemcolor} #{@inv[0].itemname} to a #{@inv[1].itemcolor} #{@inv[1].itemname} with your magical bubblegum."
+          other_players.each { |p| p.send "#{name} carefully glues a #{@inv[0].itemcolor} #{@inv[0].itemname} to a #{@inv[1].itemcolor} #{@inv[1].itemname} with his magical bubblegum."}
+          cc = @inv[0].itemcolor + " " + @inv[0].itemname
+          tt = @inv[1].itemcolor + " " + @inv[1].itemname
+          @inv.clear
+          @inv << MagicalItem.new("#{tt}", " #{cc} glued to a")
+        end  
+      end
+    end     
+        
     def prompt
       con.send_data "h:#{hp} v:#{vit}> "
       @dirty = false
     end
+    
+  end
 
-    def do_inventory
-      send "you look in your fannypack! you have #{@inventorybg} pieces of bubblegum!"
-      @inv.each { |m| send "You are balancing #{m.itemcolor} #{m.itemname} on your head."}
-      @wear.each { |m| send "You proudly wear #{m.itemcolor} #{m.itemname}."}
+  class MagicalItem
+    attr_accessor :itemname, :itemcolor
+
+    def initialize(itemname, itemcolor)
+      @itemname = itemname
+      @itemcolor = itemcolor
+      
+    end
+    
+    def to_s
+      "a #{itemcolor} #{itemname}"      
     end
   end
   
@@ -146,6 +306,8 @@ module MUD
         @player.command match[1].downcase, match[2]
       else
         @player = MUD::Player.new data.strip.capitalize, self
+  #      MUD::Players << @player
+        @player.to_room(MUD::StartRoom)  #this is the only time we'll have MUD::Room, dammit. this is the only magical room!
       end
     end
   end
